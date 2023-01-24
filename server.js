342e08acf6b25bca19069148713e092251794112
Hardik Naik <96820742+mehardiknaik@users.noreply.github.com>
2023-01-24 15:48:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,3 +25,19 @@ app.use("/api", router);
 app.listen(port, () => {
   console.log(`Server Connected to http://localhost:${port}`);
 });
+
+// Error Handler
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  const code = error.status || 500
+  const response = {
+    code,
+    message: error.message,
+  }
+  res.status(code).send(response);
+});
